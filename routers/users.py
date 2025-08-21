7b131244cae7f005b103d0f99a68c9c566a27530
Konstantinos Smanis <smanis.konstantinos@gmail.com>
2025-08-21 22:06:11 +0300

    added a phone change endpoint + renamed the "/auth" endpoint to "/users" because it creates users.

--- routers/users.py
+++ routers/users.py
@@ -7,7 +7,7 @@ from routers.auth import AuthUtils
 
 import models
 from database import get_db
-from request_response_schemas import UserVerification
+from request_response_schemas import PhoneChange, UserVerification
 from routers.auth import JwtUser
 from security import hash_password, verify_password
 
@@ -54,6 +54,49 @@ async def change_password(
         )
     else:
         db_user.hashed_password = hash_password(user_verification.new_password)  # type: ignore
+        try:
+            db_session.add(db_user)
+            db_session.commit()
+            db_session.refresh(db_user)
+
+        except IntegrityError:
+            db_session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="Constraint violation."
+            )
+
+        except SQLAlchemyError:
+            db_session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Database error.",
+            )
+
+
+@router.put("/phone-number", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(
+    phone_change: PhoneChange,
+    user: JwtUser = Depends(AuthUtils.get_current_user),
+    db_session: Session = Depends(get_db),
+):
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed."
+        )
+
+    db_user = db_session.execute(
+        select(models.Users).where(models.Users.id == user.user_id)
+    ).scalar_one_or_none()
+
+    if not verify_password(
+        submitted_password=phone_change.password,
+        password_hash=db_user.hashed_password,  # type: ignore
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Error on password change"
+        )
+    else:
+        db_user.phone_number = phone_change.new_phone_number  # type: ignore
         try:
             db_session.add(db_user)
             db_session.commit()
