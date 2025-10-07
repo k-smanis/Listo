77156cbe830a0446e9c15e333687070fb2c61594
Konstantinos Smanis <smanis.konstantinos@gmail.com>
2025-10-07 06:42:42 +0300

    setup core frontend pages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from .database import Base, engine
-from .routers import auth, tasks, admin, users
+from .routers import auth, tasks, admin, users, pages
 
 
 # Initialize App
@@ -11,7 +12,14 @@ Base.metadata.create_all(bind=engine)
 
 
 # Route to Sub-Apps
+app.include_router(pages.router)
 app.include_router(auth.router)
 app.include_router(tasks.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
+
+# Redirect to Login Page
+@app.get("/", include_in_schema=False)
+async def redirect_to_login():
+    return RedirectResponse(url="/ui/login/")
